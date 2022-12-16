21b630547e55653d2d69fff62a82cfcdb8d36cb4
aShunyaOtuski <shunya.otsuki@access-company.com>
2022-12-16 20:34:35 +0900

    add: dynamodb resources

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,14 +1,39 @@
 import * as cdk from "aws-cdk-lib";
 import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 import { Construct } from "constructs";
 
 export class CdkStack extends cdk.Stack {
+  // DB
+  userDB: dynamodb.Table;
+  mailHistoryDB: dynamodb.Table;
   // テスト対象の機能
   alertLambda: lambda.Function;
   alertApi: lambda.FunctionUrl;
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    this.userDB = new dynamodb.Table(this, "userDB", {
+      tableName: "userDB",
+      partitionKey: {
+        name: "userId",
+        type: dynamodb.AttributeType.STRING,
+      },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
+    this.mailHistoryDB = new dynamodb.Table(this, "mailHistoryDB", {
+      tableName: "mailHistoryDB",
+      partitionKey: {
+        name: "mailAddress",
+        type: dynamodb.AttributeType.STRING,
+      },
+      sortKey: {
+        name: "sendAt",
+        type: dynamodb.AttributeType.NUMBER,
+      },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
 
     this.alertLambda = new lambda.Function(this, "alertLambda", {
       functionName: "alertLambda",
