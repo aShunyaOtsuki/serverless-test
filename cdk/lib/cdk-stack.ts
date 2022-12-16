13d3a9a456b9ff086eb5631da5a68c0505487cde
aShunyaOtsuki <shunya.otsuki@access-company.com>
2022-12-16 20:25:11 +0900

    Add alert api (#2)

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,8 +1,27 @@
 import * as cdk from "aws-cdk-lib";
+import { CfnOutput } from "aws-cdk-lib";
+import * as lambda from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs";
 
 export class CdkStack extends cdk.Stack {
+  // テスト対象の機能
+  alertLambda: lambda.Function;
+  alertApi: lambda.FunctionUrl;
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    this.alertLambda = new lambda.Function(this, "alertLambda", {
+      functionName: "alertLambda",
+      code: lambda.Code.fromAsset("lambda/alert"),
+      handler: "index.handler",
+      runtime: lambda.Runtime.NODEJS_16_X,
+    });
+    this.alertApi = this.alertLambda.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE, // FIXME: 簡易的に設定している。
+    });
+    new CfnOutput(this, "alertUrl", {
+      value: this.alertApi.url,
+    });
   }
 }
