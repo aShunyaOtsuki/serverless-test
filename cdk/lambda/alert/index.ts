4cdce85efe669228fcf70413e0b490536eefe160
aShunyaOtsuki <shunya.otsuki@access-company.com>
2022-12-17 11:38:45 +0900

    Implement lambda sqs connection (#6)

--- cdk/lambda/alert/index.ts
+++ cdk/lambda/alert/index.ts
@@ -1,6 +1,7 @@
 import { APIGatewayProxyEventV2 } from "aws-lambda";
 import { DynamoDB, GetItemCommand } from "@aws-sdk/client-dynamodb";
 import { marshall, unmarshall } from "@aws-sdk/util-dynamodb";
+import { SQS, SendMessageCommand } from "@aws-sdk/client-sqs";
 
 const dynamodbClient = new DynamoDB({});
 interface IUserTable {
@@ -26,9 +27,35 @@ class UserDynamoDBTable implements IUserTable {
 }
 const UserTable = (): IUserTable => new UserDynamoDBTable();
 
+const sqsClient = new SQS({});
+interface INotifyClient {
+  notifyMessage: (message: NotifyMessage) => Promise<void>;
+}
+type NotifyMessage = {
+  mailAddress: string;
+  content: string;
+};
+
+class NotifyMessageSQS implements INotifyClient {
+  async notifyMessage(message: NotifyMessage) {
+    const sendMessageCommand = new SendMessageCommand({
+      QueueUrl: process.env.QUEUE_URL,
+      MessageBody: JSON.stringify(message),
+    });
+    await sqsClient.send(sendMessageCommand);
+    return;
+  }
+}
+const NotifyClient = () => new NotifyMessageSQS();
+
 export const handler = async (event: APIGatewayProxyEventV2) => {
   console.log(event);
   const userTable = UserTable();
   const userRecord = await userTable.getItem("test-id");
   console.log(userRecord);
+  const notifyClient = NotifyClient();
+  await notifyClient.notifyMessage({
+    mailAddress: userRecord.mailAddress,
+    content: "alert",
+  });
 };
--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -2,6 +2,8 @@ import * as cdk from "aws-cdk-lib";
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 import * as iam from "aws-cdk-lib/aws-iam";
 import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as lambdaEventSource from "aws-cdk-lib/aws-lambda-event-sources";
+import * as sqs from "aws-cdk-lib/aws-sqs";
 import { Construct } from "constructs";
 
 export class CdkStack extends cdk.Stack {
@@ -9,6 +11,8 @@ export class CdkStack extends cdk.Stack {
   userTable: dynamodb.Table;
   mailHistoryTable: dynamodb.Table;
   // テスト対象の機能
+  notifyQueue: sqs.Queue;
+  notifyLambda: lambda.Function;
   alertLambda: lambda.Function;
   alertLambdaUrl: lambda.FunctionUrl;
 
@@ -40,6 +44,18 @@ export class CdkStack extends cdk.Stack {
       MAIL_HISTORY_DYNAMO_DB_TABLE_NAMES: this.mailHistoryTable.tableName,
     };
 
+    this.notifyQueue = new sqs.Queue(this, "notifyQueue");
+    // FIXME: notifyLambdaはdummy実装
+    this.notifyLambda = new lambda.Function(this, "notifyLambda", {
+      functionName: "notify",
+      code: lambda.Code.fromAsset("lambda/notify"),
+      handler: "index.handler",
+      runtime: lambda.Runtime.NODEJS_16_X,
+    });
+    this.notifyLambda.addEventSource(
+      new lambdaEventSource.SqsEventSource(this.notifyQueue)
+    );
+
     const packagesLayer = new lambda.LayerVersion(this, "packagesLayer", {
       layerVersionName: "packages",
       code: lambda.Code.fromAsset("layer"),
@@ -51,6 +67,7 @@ export class CdkStack extends cdk.Stack {
       handler: "index.handler",
       environment: {
         ...tableNames,
+        QUEUE_URL: this.notifyQueue.queueUrl,
       },
       runtime: lambda.Runtime.NODEJS_16_X,
       layers: [packagesLayer],
@@ -60,6 +77,13 @@ export class CdkStack extends cdk.Stack {
         effect: iam.Effect.ALLOW,
         actions: ["dynamodb:GetItem"],
         resources: [this.userTable.tableArn],
+      })
+    );
+    this.alertLambda.addToRolePolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: ["sqs:SendMessage"],
+        resources: [this.notifyQueue.queueArn],
       })
     );
     this.alertLambdaUrl = this.alertLambda.addFunctionUrl({
