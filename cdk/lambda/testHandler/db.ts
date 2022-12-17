992bf9713bbdffbb7e98da97346b473ace94d6a5
aShunyaOtuski <shunya.otsuki@access-company.com>
2022-12-17 19:53:46 +0900

    imp: testHandler

--- cdk/lambda/testHandler/db.ts
+++ cdk/lambda/testHandler/db.ts
@@ -1,3 +1,16 @@
+import { UserTable, UserRecord } from "common/UserTable";
+
+const testRecord = (record: UserRecord) => {
+  return (
+    record.id === "test-id" &&
+    record.isNotifyAlert === true &&
+    record.mailAddress === "test-mailAddress"
+  );
+};
+
 export const handler = async () => {
-  return "hello world from db.ts";
+  const userTable = UserTable();
+  const record = await userTable.getItem("test-id");
+  const isSuccess = testRecord(record);
+  return isSuccess ? "Success" : "Failure";
 };
